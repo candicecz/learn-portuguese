5083c3d0aa126937eb8b1d890e76eb29bb4667ef
candicecz <czech.candice@gmail.com>
2021-07-31 01:48:18 -0400

    [REMOVE] facepaint lib

--- src/components/global/index.tsx
+++ src/components/global/index.tsx
@@ -1,14 +1,3 @@
-import facepaint from "facepaint";
-
-export const breakpoints = [576, 768, 992, 1200];
-
-// facepaint allows for media queries in this format:
-// css={mq({color: ['green', 'gray', 'hotpink']})}
-
-export const mq = facepaint(
-  breakpoints.map(bp => `@media (min-width: ${bp}px)`),
-);
-
 export {default as Box} from "./Box";
 export {default as Loading} from "./Loading";
 export {Button, IconButton} from "./Button/";
